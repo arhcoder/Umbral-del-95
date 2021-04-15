0afe10d9a8b1b9d7e524fe4d6b7ea299d34fd992
Alejandro Ramos <arhcoder@gmail.com>
2021-04-15 18:44:40 -0500

    Ahora con conocimientos sobre optimización, el algoritmo se mejoró en el conteo de muertes sobre cada porcentaje. Se redujo el tiempo de ejecución y el espacio de almacenamiento. Aún queda más por optimizar...

--- src/clases/Umbral.java
+++ src/clases/Umbral.java
@@ -10,38 +10,39 @@ public class Umbral {
         
         // Variables para la simulación:
         int repeticiones = 80000;
-        int contador, probabilidad, disparo, cantidades;
-        String estado = "Vivo";
+        int i, porcentaje, disparo;
+        boolean vivo;
         int p95 = 0, p90 = 0, p85 = 0, p80 = 0, p75 = 0, p70 = 0, p65 = 0, p60 = 0, p55 = 0, p50 = 0, p00 = 0;
         Scanner scanner = new Scanner(System.in);
+        
+        // Muertes[100] guarda en cada índice correspondiente a un porcentaje, la cantidad de muertes en dicho porcentaje //
+        int[] Muertes = new int[101];
         
         // Pregunta por la cantidad de repeticiones.
         System.out.println("¿Cuántas veces desea repetir el juego?");
         repeticiones = scanner.nextInt();
-        int[] Probabilidades = new int[repeticiones];
         
-        // Inician las 10,000 repeticiones...
+        // Inician las n repeticiones...
         System.out.println("_______________________________________\n");
-        for(contador = 0; contador < repeticiones; contador++)
+        for(i = 1; i <= repeticiones; i++)
         {
             // Reinicia las varibales, para intentar un nuevo caso.
-            estado = "Vivo";
-            probabilidad = 100;
+            vivo = true;
+            porcentaje = 100;
             
             // Ciclo en el que dispara hasta que esté muerto, y registra la probabilidad cuando murió.
-            while(estado.equals("Vivo"))
+            while(vivo)
             {
                 disparo = (int) Math.floor(Math.random()* 100 + 1);
-                if(disparo >= 1 && disparo <= probabilidad)
+                if(disparo >= 1 && disparo <= porcentaje)
                 {
-                    estado = "Vivo";
-                    probabilidad--;
+                    porcentaje--;
                 }
                 else
                 {
-                    estado = "Muerto";
-                    System.out.println("Probabilidad al morir: " + probabilidad+"% ["+((int)contador+1)+"]");
-                    Probabilidades[contador] = probabilidad;
+                    vivo = false;
+                    System.out.println("["+((int)i)+"]\t\tPorcentaje al morir: " + porcentaje + "%");
+                    Muertes[porcentaje]++;
                 }
             }
         }
@@ -50,62 +51,51 @@ public class Umbral {
         System.out.println("_______________________________________\n");
         for(int index = 100; index >= 1 ; index--)
         {
-            int n = 0;
-            for(int i = 0; i <= Probabilidades.length - 1; i++)
-            {
-                if(Probabilidades[i] == index)
-                {
-                    n++;
-                }
-            }
-            if(n > 0)
-            {
-                System.out.println("Muertes con "+index+"% = "+n);
-            }
+            System.out.println("Muertes con "+index+"% = "+Muertes[index]);
             
             if(index <= 99 && index >= 95)
             {
-                p95 += n;
+                p95 += Muertes[index];
             }
             else if(index <= 94 && index >= 90)
             {
-                p90 += n;
+                p90 += Muertes[index];
             }
             else if(index <= 89 && index >= 85)
             {
-                p85 += n;
+                p85 += Muertes[index];
             }
             else if(index <= 84 && index >= 80)
             {
-                p80 += n;
+                p80 += Muertes[index];
             }
             else if(index <= 79 && index >= 75)
             {
-                p75 += n;
+                p75 += Muertes[index];
             }
             else if(index <= 74 && index >= 70)
             {
-                p70 += n;
+                p70 += Muertes[index];
             }
             else if(index <= 69 && index >= 65)
             {
-                p65 += n;
+                p65 += Muertes[index];
             }
             else if(index <= 64 && index >= 60)
             {
-                p60 += n;
+                p60 += Muertes[index];
             }
             else if(index <= 59 && index >= 55)
             {
-                p55 += n;
+                p55 += Muertes[index];
             }
             else if(index <= 54 && index >= 50)
             {
-                p50 += n;
+                p50 += Muertes[index];
             }
             else
             {
-                p00+=n;
+                p00+=Muertes[index];
             }
         }
         System.out.println("_______________________________________\n");
